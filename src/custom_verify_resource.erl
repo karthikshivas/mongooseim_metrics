168b076f9397279a65d41c81647dc8c65a2c6be5
karthikshivas <karthikshivas@gmail.com>
2024-09-24 23:30:25 +0530

    changed logic to verify resource with muliple rids in message and also to send message stanza with type error instead of error stanza

--- src/custom_verify_resource.erl
+++ src/custom_verify_resource.erl
@@ -30,36 +30,50 @@ user_send_message(Acc, Params, _) ->
 
 
 verify_rid(From, To, Packet, Acc) ->
-    Rid = exml_query:path(Packet, 
-        [{element, <<"encrypted">>}, 
-         {element, <<"header">>}, {element, <<"key">>}, {attr, <<"rid">>}], <<>>),     
-    {jid, LUser, LServer, _} = To,    
-    % ?LOG_NOTICE(#{what => testing, to => To, rid => Rid, luser => LUser, lserver => LServer}),
-    case Rid of
-        <<>> -> {ok, Acc};
-        _ -> 
-            Payloads = mod_pubsub_db_backend:get_user_payloads(LUser, LServer),
-            % ?LOG_NOTICE(#{what => testing, payloads => Payloads}),
-            case Payloads of
-                [[<<"eu.siacs.conversations.axolotl.devicelist">>, _, DeviceList] | _] ->
-                    % ?LOG_NOTICE(#{what => testing, devicelist => DeviceList}),
-                    {ok, DeviceListXML} = exml:parse(DeviceList),
-                    DeviceElements = exml_query:subelements(DeviceListXML, <<"device">>),
-                    DeviceIDs = [exml_query:attr(Device, <<"id">>, undefined) || Device <- DeviceElements],
-                    case lists:member(Rid, DeviceIDs) of
-                        true -> {ok, Acc};
-                        _ ->
-                            ErrorStanza = mongoose_xmpp_errors:item_not_found(<<"en">>, 
-                                    <<"Incorrect Device ID">>),
-                            ejabberd_router:route(server_jid(From), From, ErrorStanza),     
-                            {stop, Acc}   
-                    end;
-                _ -> 
-                    ErrorStanza = mongoose_xmpp_errors:item_not_found(<<"en">>, 
-                                    <<"Device list not found for this user">>),
-                    ejabberd_router:route(server_jid(From), From, ErrorStanza),     
-                    {stop, Acc}   
-            end
+    ?LOG_NOTICE(#{what => testing, packet => Packet}),
+    try 
+        Header = exml_query:path(Packet, 
+            [{element, <<"encrypted">>}, 
+            {element, <<"header">>}]),
+        KeyElements = exml_query:subelements(Header, <<"key">>),    
+        Rids0 = [exml_query:attr(Key, <<"rid">>, undefined) || Key <- KeyElements],   
+        Rids = lists:filter(fun(Rid) -> Rid =/= undefined end, Rids0),
+
+        {jid, LUser, LServer, _} = To,    
+        % ?LOG_NOTICE(#{what => testing, to => To, rid => Rid, luser => LUser, lserver => LServer}),
+        case Rids of
+            [] -> {ok, Acc};
+            _ -> 
+                Payloads = mod_pubsub_db_backend:get_user_payloads(LUser, LServer),
+                % ?LOG_NOTICE(#{what => testing, payloads => Payloads}),
+                case Payloads of
+                    [[<<"eu.siacs.conversations.axolotl.devicelist">>, _, DeviceList] | _] ->
+                        % ?LOG_NOTICE(#{what => testing, devicelist => DeviceList}),
+                        {ok, DeviceListXML} = exml:parse(DeviceList),
+                        DeviceElements = exml_query:subelements(DeviceListXML, <<"device">>),
+                        DeviceIDs = [exml_query:attr(Device, <<"id">>, undefined) || Device <- DeviceElements],
+                        case lists:all(fun(Rid) -> lists:member(Rid, DeviceIDs) end, Rids) of
+                            true -> {ok, Acc};
+                            _ ->
+                                Lang = <<"en">>,
+                                ErrText = <<"Incorrect Device ID">>,
+                                ErrorStanza = jlib:make_error_reply(
+                                    Packet, mongoose_xmpp_errors:item_not_found(Lang, ErrText)),
+                                % ?LOG_NOTICE(#{what => testing, error_stanza => ErrorStanza}),
+                                ejabberd_router:route(server_jid(From), From, ErrorStanza),     
+                                {stop, Acc}   
+                        end;
+                    _ -> 
+                        ErrorStanza = jlib:make_error_reply(Packet, mongoose_xmpp_errors:item_not_found(<<"en">>, 
+                                        <<"Device list not found for this user">>)),
+                        ejabberd_router:route(server_jid(From), From, ErrorStanza),     
+                        {stop, Acc}   
+                end
+        end
+    catch
+    _:Error ->
+        ?LOG_ERROR(#{what => rid_extraction_failed, error => Error}),
+            {ok, Acc}
     end.
 
 server_jid(#jid{lserver = Host}) ->
